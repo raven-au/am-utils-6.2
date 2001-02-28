960fa1e9382003ca9384e25fbf9e32b79d80a820
Erez Zadok <ezk@cs.sunysb.edu>
2001-02-28 23:13:43 +0000

    * Makefile.am: removed unused conf/trap/trap_hpux11.h

--- conf/trap/trap_hpux11.h
+++
@@ -1,3 +0,0 @@
-/* $srcdir/conf/trap/trap_hpux11.h */
-extern int mount_hpux11(const char *fsname, const char *dir, int flags, MTYPE_TYPE type, caddr_t data);
-#define MOUNT_TRAP(type, mnt, flags, mnt_data) mount_hpux11(mnt->mnt_fsname, mnt->mnt_dir, flags, type, mnt_data)
